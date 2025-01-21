5a21b8368939cdcd4407c25f748a69690796dbe7
Mahmoud Abdel salam <mahmoudabdelsalam.alpha@gmail.com>
2025-01-21 11:00:55 +0200

    adding geo location using latitude and longitude

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(
+      `https://www.google.com/maps/place/31%C2%B011'40.0%22N+30%C2%B005'12.3%22E/@${latitude},${longitude}`
+    );
+  },
+  function () {
+    alert('Could not get your position');
+  }
+);
