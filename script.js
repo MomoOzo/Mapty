f8594e3f1c7731ef86602580fcf4b122c090ff1b
Mahmoud Abdel salam <mahmoudabdelsalam.alpha@gmail.com>
2025-01-21 11:28:27 +0200

    adding map using leaflet

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ navigator.geolocation.getCurrentPosition(
     console.log(
       `https://www.google.com/maps/place/31%C2%B011'40.0%22N+30%C2%B005'12.3%22E/@${latitude},${longitude}`
     );
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
   },
   function () {
     alert('Could not get your position');
